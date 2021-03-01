137bf6c14caf99089f7c2a352e199f2592cf79d4
maxschool-doe <69950806+maxschool-doe@users.noreply.github.com>
2021-03-01 14:02:07 +1100

    add better spacing

--- eventselect.py
+++ eventselect.py
@@ -1,6 +1,7 @@
 print("Module EventSelect loaded.")
 from app import app, savefile, openfile
 import flask
+
 @app.route("/eventselect")
 def eventselect():
     return flask.render_template("eventselect.html",students=)
