9c9cb9ea2b5aad5a0e873d8d3a5f4d40c776d5c4
maxschool-doe <69950806+maxschool-doe@users.noreply.github.com>
2021-03-05 14:39:27 +1100

    Update choosestudents.py

--- choosestudents.py
+++ choosestudents.py
@@ -7,16 +7,17 @@ import json
 def choosestudents(eventid):
     data = loaddata()
     if "events" in data and eventid in data["events"] and data["events"][eventid] != None:
-        for student in data["students"]:
-            if data["students"][agegroup]["students"][student]["prefname"]:
-                student_name = data["students"][agegroup]["students"][student]["prefname"]
-            else:
-                student_name = data["students"][agegroup]["students"][student]["firstname"]+" "+data["students"][agegroup]["students"][student]["lastname"]
-            
-            if student in data["events"][eventid]["entrants"]:
-                students.append([student_name,student,int(data["events"][eventid]["entrants"]["heat"])+1,getuid()])
-            else:
-                students.append([student_name,student,"",getuid()])
+        for agegroup in data["students"]:
+            for student in data["students"][agegroup]["students"]:
+                if data["students"][agegroup]["students"][student]["prefname"]:
+                    student_name = data["students"][agegroup]["students"][student]["prefname"]
+                else:
+                    student_name = data["students"][agegroup]["students"][student]["firstname"]+" "+data["students"][agegroup]["students"][student]["lastname"]
+                # student_name = data["students"][agegroup]["students"][student]["firstname"]+" "+data["students"][agegroup]["students"][student]["lastname"]
+                if student in data["events"][eventid]["entrants"]:
+                    students.append([student_name,student,int(data["events"][eventid]["entrants"]["heat"])+1,getuid()])
+                else:
+                    students.append([student_name,student,"",getuid()])
         return flask.render_template("choosestudents.html",eventid=eventid,students=sorted(students))
     else:
         return "Event not found"
