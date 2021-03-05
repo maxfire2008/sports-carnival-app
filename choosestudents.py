e1be4370048e5660c302d52e4ec53d3c01821daa
maxschool-doe <69950806+maxschool-doe@users.noreply.github.com>
2021-03-05 14:35:55 +1100

    updated mock and redid syntax and code and made based on student not heat

--- choosestudents.py
+++ choosestudents.py
@@ -6,42 +6,28 @@ import json
 @app.route("/choosestudents/<eventid>")
 def choosestudents(eventid):
     data = loaddata()
-    if "events" in data and eventid in data["events"]:
-        studentsheats = {}
-        allstudentsselected = []
-        for heat in data["events"][eventid]["heats"]:
-            inheat = []
-            for student in data["events"][eventid]["heats"][heat]:
-                if data["events"][eventid]["heats"][heat][student] != None:
-                    inheat.append(student)
-            studentsheats[heat] = inheat
-            allstudentsselected+=inheat
-        students = []
-        for agegroup in data["students"]:
-            if (data["events"][eventid]["gender"] == "*" or data["events"][eventid]["gender"] == data["students"][agegroup]["gender"]) and (data["events"][eventid]["year"] == "*" or data["events"][eventid]["year"] == data["students"][agegroup]["year"]):
-                for student in data["students"][agegroup]["students"]:
-                    if data["students"][agegroup]["students"][student]["prefname"]:
-                        student_name = data["students"][agegroup]["students"][student]["prefname"]
-                    else:
-                        student_name = data["students"][agegroup]["students"][student]["firstname"]+" "+data["students"][agegroup]["students"][student]["lastname"]
-                    if student in allstudentsselected:
-                        for heatname in studentsheats:
-                            if student in studentsheats[heatname]:
-                                break
-                        students.append([student_name,student,int(heatname)+1,getuid()])
-                    else:
-                        students.append([student_name,student,"",getuid()])
+    if "events" in data and eventid in data["events"] and data["events"][eventid] != None:
+        for student in data["students"]:
+            if data["students"][agegroup]["students"][student]["prefname"]:
+                student_name = data["students"][agegroup]["students"][student]["prefname"]
+            else:
+                student_name = data["students"][agegroup]["students"][student]["firstname"]+" "+data["students"][agegroup]["students"][student]["lastname"]
+            
+            if student in data["events"][eventid]["entrants"]:
+                students.append([student_name,student,int(data["events"][eventid]["entrants"]["heat"])+1,getuid()])
+            else:
+                students.append([student_name,student,"",getuid()])
         return flask.render_template("choosestudents.html",eventid=eventid,students=sorted(students))
     else:
         return "Event not found"
 
-@app.route("/savestudents/<eventid>",methods = ['POST'])
-def savestudents(eventid):
-    print(flask.request.data.decode())
-    requestdata = json.loads(flask.request.data.decode())
-    currentdata = loaddata()
-    newdata = currentdata[eventid]["heats"]
-    for student in requestdata:
-        if requestdata["student"] != "":
-            
-    return ""
+# @app.route("/savestudents/<eventid>",methods = ['POST'])
+# def savestudents(eventid):
+    # print(flask.request.data.decode())
+    # requestdata = json.loads(flask.request.data.decode())
+    # currentdata = loaddata()
+    # newdata = currentdata[eventid]["heats"]
+    # for student in requestdata:
+        # if requestdata["student"] != "":
+            # None
+    # return ""
