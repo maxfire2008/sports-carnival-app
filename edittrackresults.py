a3404948234e0f4432546756643206fe221eac8d
maxschool-doe <69950806+maxschool-doe@users.noreply.github.com>
2021-03-12 14:36:17 +1100

    added cool stuff Damn i hate descriptions for pushes

--- edittrackresults.py
+++ edittrackresults.py
@@ -3,19 +3,45 @@ from app import app, savefile, openfile, loaddata, getuid
 import flask
 import json
 
+def intomsms(t):
+    m=int(t/60000)
+    s=int((t-(m*60000))/1000)
+    ms=(t-(m*60000)-(s*1000))
+    return m,s,ms
+
 @app.route("/edittrackresults/<eventid>")
 def edittrackresults(eventid):
     data = loaddata()
     if "events" in data and eventid in data["events"] and data["events"][eventid] != None:
         heats = {}
         for entrant in data["events"][eventid]["entrants"]:
-            if entrant != None:
-                if entrant["heat"] not in heats:
-                    heats[entrant["heat"]] = []
+            if data["events"][eventid]["entrants"][entrant] != None:
+                if data["events"][eventid]["entrants"][entrant]["heat"] not in heats:
+                    heats[data["events"][eventid]["entrants"][entrant]["heat"]] = []
                 timeminutes = ""
                 timeseconds = ""
                 timemilliseconds = ""
-                students.append()
+                rank = ""
+                if data["events"][eventid]["entrants"][entrant]["type"] == "time":
+                    timeminutes,timeseconds,timemilliseconds = intomsms(data["events"][eventid]["entrants"][entrant]["time"])
+                    timeminutes=str(timeminutes)
+                    timeseconds=str(timeseconds)
+                    timemilliseconds=str(timemilliseconds)
+                if data["events"][eventid]["entrants"][entrant]["type"] == "rank":
+                    rank = data["events"][eventid]["entrants"][entrant]["rank"]
+                timestring = timeminutes+":"+timeseconds+":"+timemilliseconds
+                if entrant in data["events"][eventid]["entrants"] and data["events"][eventid]["entrants"][entrant] != None and "display" in data["events"][eventid]["entrants"][entrant]:
+                    studentdisplaylocation = int(data["events"][eventid]["entrants"][entrant]["display"])
+                else:
+                    studentdisplaylocation = 10000000
+                for agegroup in data["students"]:
+                    for student in data["students"][agegroup]["students"]:
+                        if student == entrant:
+                            if data["students"][agegroup]["students"][entrant]["prefname"]:
+                                student_name = data["students"][agegroup]["students"][entrant]["prefname"]
+                            else:
+                                student_name = data["students"][agegroup]["students"][entrant]["firstname"]+" "+data["students"][agegroup]["students"][student]["lastname"]
+                heats[data["events"][eventid]["entrants"][entrant]["heat"]].append([studentdisplaylocation,entrant,student_name,timestring,rank])
         return flask.render_template("edittrackresults.html",eventid=eventid,eventname=data["events"][eventid]["name"])
     else:
         return "Event not found"
